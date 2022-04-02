0b9d473a1e1ec68c97752648c28356ab8108b821
Laudelino M. C. Neto <fysout@gmail.com>
2022-04-01 21:21:48 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -0,0 +1,24 @@
+package br.com.senai.manutencaosenaiapi;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InitApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitApp.class, args);
+		
+	}	
+	
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ac) {
+		return args -> {
+			System.out.println("Deu certo! ");
+		};
+	}
+
+}
---
+++ src/test/java/br/com/senai/manutencaosenaiapi/InitAppTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.manutencaosenaiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
