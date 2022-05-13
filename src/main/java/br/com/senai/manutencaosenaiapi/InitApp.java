dd304d74b7a122b63f392fdf52935e08cc44be2b
laudelino.cardoso <laudelino@farolshopping.com.br>
2022-05-13 15:00:25 -0300

    Atualização do projeto para remoção de elementos presentes em outra branch

--- src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
+++ src/main/java/br/com/senai/manutencaosenaiapi/InitApp.java
@@ -1,45 +1,23 @@
 package br.com.senai.manutencaosenaiapi;
 
-import java.awt.EventQueue;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import br.com.senai.manutencaosenaiapi.view.TelaCadastroDeTipo;
-
 @SpringBootApplication
 public class InitApp {
-	
-	@Autowired
-	private TelaCadastroDeTipo telaDeCadastroDeTipo;
 
 	public static void main(String[] args) {
-		SpringApplicationBuilder builder = 
-				new SpringApplicationBuilder(InitApp.class);
-		builder.headless(false);
-		builder.run(args);
+		SpringApplication.run(InitApp.class, args);
 	}
 			
 	@Bean	
 	public CommandLineRunner commandLineRunner(ApplicationContext ac) {
 		return args -> {
 			try {				
-				
-				EventQueue.invokeLater(new Runnable() {
-					public void run() {
-						try {							
-							telaDeCadastroDeTipo.setVisible(true);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				});
-				
+										
 			}catch (Exception e) {				
 				System.out.println(e.getMessage());
 			}
